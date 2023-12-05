5c2039c83c8c769b6766421b3def26b098979820
Aleksandr Krotov <73738250+PhlegmaticOne@users.noreply.github.com>
2023-12-05 07:54:09 +0300

    Logger

---
+++ PhlegmaticOne.FruitNinja.Server/Log/ILogger.cs
@@ -0,0 +1,10 @@
+﻿namespace PhlegmaticOne.FruitNinja.Server.Log;
+
+public interface ILogger
+{
+    bool IsVerbose { get; set; }
+    void LogInfo(string message);
+    void LogInfo(int playerId, string message);
+    void LogImportantInfo(int playerId, string message);
+    void LogError(int playerId, string message);
+}
---
+++ PhlegmaticOne.FruitNinja.Server/Log/LoggerConsole.cs
@@ -0,0 +1,47 @@
+﻿namespace PhlegmaticOne.FruitNinja.Server.Log;
+
+public class LoggerConsole : ILogger
+{
+    public bool IsVerbose { get; set; } = true;
+
+    public void LogInfo(string message)
+    {
+        if (!IsVerbose)
+        {
+            return;
+        }
+
+        Console.WriteLine(message);
+    }
+
+    public void LogInfo(int playerId, string message)
+    {
+        if (!IsVerbose)
+        {
+            return;
+        }
+
+        LogImportantInfo(playerId, message);
+    }
+
+    public void LogImportantInfo(int playerId, string message)
+    {
+        Console.Write($"Player {playerId}: ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+
+    public void LogError(int playerId, string message)
+    {
+        if (!IsVerbose)
+        {
+            return;
+        }
+
+        Console.Write($"Player {playerId}: ");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+}
